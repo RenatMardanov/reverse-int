aeea8cf0e3c8198a97b577e3342021900618a4c3
Renat <rent1995@ya.ru>
2022-12-27 14:03:43 +0700

    feat: task complite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    for(let i = 1; i <= n.toString().length; i++) {
+        if(n.toString().slice(-i) < 1) {
+            continue
+        }else {
+            result = result + n.toString().at(-i)            
+        }
+    }
+    return +result
 }
+
